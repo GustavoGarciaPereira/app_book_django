063a156e363cac3cc6b46baabc116424b46e53f5
gustavogarciapereira <gusgurtavo@gmail.com>
2025-02-21 20:16:38 -0300

    fix:correcao do bug de urls logout

--- books_preject/books/urls.py
+++ books_preject/books/urls.py
@@ -2,14 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 from .views import BookListView, BookCreateView, BookUpdateView
-from .views import signup
 urlpatterns = [
     path('', BookListView.as_view(), name='book_list'),
     path('book_create/', BookCreateView.as_view(), name='book_create'),
-path('book_edit/<int:pk>/', BookUpdateView.as_view(), name='book_edit'),  # Add this line
-    path('signup/', signup, name='signup'),
+    path('book_edit/<int:pk>/', BookUpdateView.as_view(), name='book_edit'),  # Add this line
 
-    #quero fazer o login e o logout
-    path('login/', include('django.contrib.auth.urls'), name='login'),
-    path('logout/', include('django.contrib.auth.urls'), name='logout'),
+    #quero fazer o login e o gustavo_g
+
 ]
--- books_preject/books_preject/urls.py
+++ books_preject/books_preject/urls.py
@@ -17,7 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from books.views import signup
+from django.contrib.auth import views as auth_views  # Importe as views de autenticação
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('books.urls')),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # URL de logout
+    path('login/', auth_views.LoginView.as_view(), name='login'),  # URL de login
+    path('signup/', signup, name='signup'),
 ]
